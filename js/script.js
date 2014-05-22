f573c53f84ac57a4ffe73516498eff8303fc0dcb
puhrome <phirom.yim@fullsail.edu>
2014-05-22 19:27:15 -0400

    Conditionals Industry Based
    Using if and else statements to create a conditional with alerts and
    prompts

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Conditionals Personal Created by Phirom Yim on 5/22/14.
+ */
+
+var memoryCard = 16;//define memory card gigs for DSLR
+//user input how large the files are in gigs
+var photos = Number (prompt ("How many photos have you taken?"));
+//create a variable to calculate the amount left by the percentage of new files will take up in hard drive
+//total storage is files divided by hard drive and multiplied by 100 to get percentage
+if (photos){// if condition is true
+    console.log ("You have" + photos + " gigs of storage");//print to console
+}else{// if condition is null;
+    alert("Please enter a valid number");//alert a user input
+}
+var photoRaw = 40 * memoryCard; // rough estimated number of RAW photos that a 16 gig memory card hold
+var storageLeft = photoRaw - photos;//the storage needed is the raw photos minus user input amount file gigs
+//create variable expression for calculating the storage percentage
+//storage percent is calculated by the amount of needed storage by 16 gig times 100
+var storagePercent = storageLeft / photoRaw * 100;
+//create a variable to calculates how much storage is being used
+//to find out how much storage is used from 16gig (100%) subtract storage percent by 100
+var storageUsed = 100 - storagePercent;
+//create a variable that calculates how much is left on the hard drive
+//16 gig is 100 percent and subtract how many photos taken to get remaining photos left
+var memSpace = 100 - storageUsed;
+//create ternary to print if true or else statement if false
+(memSpace >= storageUsed) ? console.log("Whew! You can save all of your hard work in one place!"): console.log("Sorry you are at" + " " + driveSpace + "% total storage");
+//create alert for statement of how much storage is left on memory card
+alert("You have " + " " + memSpace + "% total storage left");//alert how much space is left
