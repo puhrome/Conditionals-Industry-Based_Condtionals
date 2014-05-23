19bd723e183e08e1cda6b31be35d1dc5332bf47d
puhrome <phirom.yim@fullsail.edu>
2014-05-22 22:50:54 -0400

    Update script.js
    Added additional notes ternary.

--- js/script.js
+++ js/script.js
@@ -8,9 +8,10 @@ var photos = Number (prompt ("How many photos have you taken?"));
 //create a variable to calculate the amount left by the percentage of new files will take up in hard drive
 //total storage is files divided by hard drive and multiplied by 100 to get percentage
 if (photos){// if condition is true
-    console.log ("You have" + photos + " gigs of storage");//print to console
-}else{// if condition is null;
-    alert("Please enter a valid number");//alert a user input
+    console.log ("You have taken " + photos);//print to console
+}else if(photos < 1 || photos > 640){//if condition is false and number is between 1 and 50
+    console.log("You entered an invalid number");//print to console
+    alert("You entered a number less than 1 or greater than 640");//alert string
 }
 var photoRaw = 40 * memoryCard; // rough estimated number of RAW photos that a 16 gig memory card hold
 var storageLeft = photoRaw - photos;//the storage needed is the raw photos minus user input amount file gigs
@@ -24,6 +25,6 @@ var storageUsed = 100 - storagePercent;
 //16 gig is 100 percent and subtract how many photos taken to get remaining photos left
 var memSpace = 100 - storageUsed;
 //create ternary to print if true or else statement if false
-(memSpace >= storageUsed) ? console.log("Whew! You can save all of your hard work in one place!"): console.log("Sorry you are at" + " " + driveSpace + "% total storage");
+(memSpace >= storageUsed) ? console.log("You have enough memory for more photos"): console.log("Sorry you are at" + " " + storagePercent + "% total storage");
 //create alert for statement of how much storage is left on memory card
-alert("You have " + " " + memSpace + "% total storage left");//alert how much space is left
+alert("You have " + " " + storagePercent + "% total storage left");//alert how much space is left
